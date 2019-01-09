462a7477607a34907bbd6edf04e21851409ab061
KayinCheung <cheungkayin00@gmail.com>
2019-01-10 00:54:30 +0800

    ~

---
+++ precache-manifest.6d8d5e65779704d0991f1a27d72b7380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe1/tictactoe1.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "af0848d1e9b7dedae4ef",
+    "url": "/tictactoe1/tictactoe1.github.io/static/js/runtime~main.af0848d1.js"
+  },
+  {
+    "revision": "bc3094e8fe08d9193f6f",
+    "url": "/tictactoe1/tictactoe1.github.io/static/js/main.bc3094e8.chunk.js"
+  },
+  {
+    "revision": "06370080ed253be4ebe5",
+    "url": "/tictactoe1/tictactoe1.github.io/static/js/1.06370080.chunk.js"
+  },
+  {
+    "revision": "bc3094e8fe08d9193f6f",
+    "url": "/tictactoe1/tictactoe1.github.io/static/css/main.dabeb52d.chunk.css"
+  },
+  {
+    "revision": "0b1a886ddbeaaa21ad2e2b8b18a85546",
+    "url": "/tictactoe1/tictactoe1.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.05cfba731d67dbbfe57a4f678f3458ee.js"
+  "/tictactoe1/tictactoe1.github.io/precache-manifest.6d8d5e65779704d0991f1a27d72b7380.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tictactoe1/tictactoe1.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
